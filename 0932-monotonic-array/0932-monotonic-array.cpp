6fc891f212534a7e459db0b246bf0959c0d31fdf
sumanadhikari0625 <144775727+sumanadhikari0625@users.noreply.github.com>
2023-09-19 12:11:04 +0530

    Time: 141 ms (20.39%), Space: 96.8 MB (11.2%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.cpp
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
         int n= nums.size();
-        bool inc= false, dec= false;
-        for(int i= 0; i<n-1;++i){
-            if(nums[i]>nums[i+1]) dec= true;
-            else if(nums[i]<nums[i+1]) inc= true;
+        bool dec= false, inc= false;
+        for(int i= 0; i<n-1; ++i){
+            if(nums[i]<nums[i+1]) inc= true;
+            else if(nums[i]>nums[i+1]) dec= true;
         }
-        if(dec && inc) return false;
+        if(inc && dec) return false;
         return true;
-        // we check if the array is increasing or decreasing if the array is both increasing and decreasing then we return false and true other wise
-
-        
     }
 };
