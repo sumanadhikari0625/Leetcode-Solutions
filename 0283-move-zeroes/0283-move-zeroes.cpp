0807cd53474f625194eb9cf78a31100979da98c1
sumanadhikari0625 <144775727+sumanadhikari0625@users.noreply.github.com>
2023-09-21 06:12:21 +0530

    Time: 32 ms (14.56%), Space: 20.5 MB (5.2%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        // int zero=0,nonzero=0;
+        int n=nums.size();
+        vector<int>v;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                v.push_back(nums[i]);
+                //break;
+            }
+        }
+        for(int i=0;i<v.size();i++)
+        {
+            nums[i]=v[i];
+        }
+        for(auto it:nums) cout<<it<<" ";
+        int t=v.size();
+        for(int i=t;i<n;i++)
+        {
+            nums[i]=0;
+        }
+        return;
+    }
+};
