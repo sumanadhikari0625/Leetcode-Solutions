2ed892cdff9ccb9b8f0bd0f8d6e48f813b96174f
sumanadhikari0625 <144775727+sumanadhikari0625@users.noreply.github.com>
2023-09-23 08:00:53 +0530

    Time: 3 ms (65.05%), Space: 12.3 MB (71.88%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    vector<string> valid;
+
+    void gen(string &s,int open,int close)
+    {
+        if(open==0 && close==0)
+        {
+            valid.push_back(s);
+            return ;
+        }
+        if(open>0)
+        { 
+            s.push_back('(');
+            gen(s,open-1,close);
+            s.pop_back();
+        }
+        if(close>0)
+        {
+            if(open<close) {
+                s.push_back(')');
+                gen(s,open,close-1);
+                s.pop_back();
+            }
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        string s;
+        gen(s,n,n);
+        return valid;
+    }
+};
