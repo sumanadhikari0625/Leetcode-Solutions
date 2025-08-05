a5f06688b8cbd2526c45b2b58334515df6ae2925
suman adhikari <144775727+sumanadhikari0625@users.noreply.github.com>
2025-08-05 09:44:04 +0530

    Time: 0 ms (100%), Space: 11.7 MB (42.3%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        map<int,int>mpp;int i=0;
-        for(auto it:nums){mpp[it]++;}
-        for(auto it:mpp){
-            if(it.first==val)continue;
-            else{
-                for(int j=0;j<it.second;j++)
-                nums[i++]=it.first;
-
+        int i=0;
+        for(int j=0;j<nums.size();j++)
+        {
+            if(nums[j]!=val)
+            {
+                nums[i]=nums[j];
+                i++;
             }
         }
-        return nums.size()-mpp[val];
+        return i;
     }
 };
